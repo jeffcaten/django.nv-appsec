412febbd5ee96cc5d6bd7c538f8f53e22f90c27e
jeffcaten <jeffcaten@gmail.com>
2020-07-13 15:36:14 +0000

    Initial commit

---
+++ taskManager/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for projmanager project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+import trend_app_protect.start
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskManager.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
